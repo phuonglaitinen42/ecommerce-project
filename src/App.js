df24531e25646ae3fedf55a26a756cffa7e73475
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-07-30 14:04:54 +0300

    update menu iutems to navigate with withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
+import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
