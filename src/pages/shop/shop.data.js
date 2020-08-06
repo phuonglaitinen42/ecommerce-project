16af3929ff540e4723bc9b931221b2f3e5df1688
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-08-06 22:14:21 +0300

    customized some data to be more personalized

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -1,31 +1,35 @@
 const SHOP_DATA = [
   {
     id: 1,
-    title: "Hats",
-    routeName: "hats",
+    title: "K-Beauty",
+    routeName: "k-beauty",
     items: [
       {
         id: 1,
-        name: "Brown Brim",
-        imageUrl: "https://i.ibb.co/ZYW3VTp/brown-brim.png",
-        price: 25,
+        name: "Low pH Good Morning Gel Cleaser",
+        imageUrl:
+          "https://s1.thcdn.com/productimg/1600/1600/11401192-1824651979604903.jpg",
+        price: 19,
       },
       {
         id: 2,
-        name: "Blue Beanie",
-        imageUrl: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
+        name: "BHA Blackhead Power Liquid",
+        imageUrl:
+          "https://www.oletkaunis.fi/WebRoot/vilkasfi02/Shops/2015081901/5E1E/BD52/32F9/9FA8/D6AF/0A28/1011/7FA6/BHA_Blackhead_Power_Liquid_ml.jpg",
         price: 18,
       },
       {
         id: 3,
-        name: "Brown Cowboy",
-        imageUrl: "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
+        name: "From Green Cleansing Oil",
+        imageUrl:
+          "https://d1flfk77wl2xk4.cloudfront.net/Assets/GalleryImage/12/362/L_g0121536212.jpg",
         price: 35,
       },
       {
         id: 4,
-        name: "Grey Brim",
-        imageUrl: "https://i.ibb.co/RjBLWxB/grey-brim.png",
+        name: "Jeju Cherry Blossom Tone-up Cream",
+        imageUrl:
+          "https://shopichigo.com/wp-content/uploads/2019/04/Innisfree-Jeju-Cherry-Blossom-Tone-Up-Cream2.jpg",
         price: 25,
       },
       {
@@ -62,32 +66,35 @@ const SHOP_DATA = [
   },
   {
     id: 2,
-    title: "Sneakers",
-    routeName: "sneakers",
+    title: "J-Beauty",
+    routeName: "j-beauty",
     items: [
       {
         id: 10,
-        name: "Adidas NMD",
-        imageUrl: "https://i.ibb.co/0s3pdnc/adidas-nmd.png",
-        price: 220,
+        name: "Biore UV Aqua Rich",
+        imageUrl:
+          "https://www.takaski.com/wp-content/uploads/2017/02/KAO-Biore-UV-Aqua-Rich-Watery-Japanese-Essence-2017-Back_bottler-1.jpg",
+        price: 15,
       },
       {
         id: 11,
-        name: "Adidas Yeezy",
-        imageUrl: "https://i.ibb.co/dJbG1cT/yeezy.png",
-        price: 280,
+        name: "Gokujyun Hydrating Lotion",
+        imageUrl: "https://s3.images-iherb.com/rto/rto15571/l/0.jpg",
+        price: 30,
       },
       {
         id: 12,
-        name: "Black Converse",
-        imageUrl: "https://i.ibb.co/bPmVXyP/black-converse.png",
-        price: 110,
+        name: "Softymo Speedy Cleansing Oil",
+        imageUrl:
+          "https://images-na.ssl-images-amazon.com/images/I/71zXXa3gmQL._SL1500_.jpg",
+        price: 15,
       },
       {
         id: 13,
-        name: "Nike White AirForce",
-        imageUrl: "https://i.ibb.co/1RcFPk0/white-nike-high-tops.png",
-        price: 160,
+        name: "SK-II Facial Treatment Essence",
+        imageUrl:
+          "https://www.takaski.com/wp-content/uploads/2016/04/SK-II-Japanese-Facial-Treatment-Essence.jpg",
+        price: 80,
       },
       {
         id: 14,
@@ -117,32 +124,36 @@ const SHOP_DATA = [
   },
   {
     id: 3,
-    title: "Jackets",
-    routeName: "jackets",
+    title: "The Ordinary",
+    routeName: "theordinary",
     items: [
       {
         id: 18,
-        name: "Black Jean Shearling",
-        imageUrl: "https://i.ibb.co/XzcwL5s/black-shearling.png",
-        price: 125,
+        name: "AHA 30% + BHA 2% Peeling Solution",
+        imageUrl:
+          "https://cdn.cultbeauty.com/slots-img/ord/ord011_theordinary_aha30bha2peelingsolution_1_1560x1960-6iwul.jpg",
+        price: 14,
       },
       {
         id: 19,
-        name: "Blue Jean Jacket",
-        imageUrl: "https://i.ibb.co/mJS6vz0/blue-jean-jacket.png",
-        price: 90,
+        name: "Niacinamide 10% + Zinc 1%",
+        imageUrl:
+          "https://www.sephora.com/productimages/sku/s2031391-main-zoom.jpg",
+        price: 9,
       },
       {
         id: 20,
-        name: "Grey Jean Jacket",
-        imageUrl: "https://i.ibb.co/N71k1ML/grey-jean-jacket.png",
-        price: 90,
+        name: "Glycolic Acid 7% Toning Solution",
+        imageUrl:
+          "https://cdn.cocopanda.fi/Media/Product/Image/900/7fead384-5449-4d85-9b3a-22c00a793c1b_1.jpg",
+        price: 10,
       },
       {
         id: 21,
-        name: "Brown Shearling",
-        imageUrl: "https://i.ibb.co/s96FpdP/brown-shearling.png",
-        price: 165,
+        name: "Salicylic Acid 2% Masque",
+        imageUrl:
+          "https://www.bangerhead.fi/bilder/artiklar/zoom/B055328_1.jpg?m=1569314780",
+        price: 13,
       },
       {
         id: 22,
@@ -154,32 +165,36 @@ const SHOP_DATA = [
   },
   {
     id: 4,
-    title: "Womens",
-    routeName: "womens",
+    title: "Drugstore Skincare",
+    routeName: "drugstore",
     items: [
       {
         id: 23,
-        name: "Blue Tanktop",
-        imageUrl: "https://i.ibb.co/7CQVJNm/blue-tank.png",
-        price: 25,
+        name: "CeraVe Hydrating Facial Cleanser",
+        imageUrl:
+          "https://images.ulta.com/is/image/Ulta/2241933?op_sharpen=1&resMode=bilin&qlt=85&wid=800&hei=800&fmt=jpg",
+        price: 13,
       },
       {
         id: 24,
-        name: "Floral Blouse",
-        imageUrl: "https://i.ibb.co/4W2DGKm/floral-blouse.png",
+        name: "La Roche-Posay Effaclar Duo [+] ",
+        imageUrl:
+          "https://static.beautytocare.com/media/catalog/product/cache/global/image/650x650/85e4522595efc69f496374d01ef2bf13/l/a/la-roche-posay-effaclar-duo-corrective-unclogging-care-40ml.jpg",
         price: 20,
       },
       {
         id: 25,
-        name: "Floral Dress",
-        imageUrl: "https://i.ibb.co/KV18Ysr/floral-skirt.png",
-        price: 80,
+        name: "Avène Cicalfate+ Repair Cream",
+        imageUrl:
+          "https://www.apteekkituotteet.fi/WebRoot/Euran/Shops/Eura/46B8/452D/909D/9220/3DC9/C0A8/071C/E60A/Avene_cicalfaterepair_ml.jpg",
+        price: 6,
       },
       {
         id: 26,
-        name: "Red Dots Dress",
-        imageUrl: "https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png",
-        price: 80,
+        name: "Vichy Mineral 89 Daily Booster",
+        imageUrl:
+          "https://www.yliopistonapteekki.fi/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/2/3/2382745.jpg",
+        price: 24,
       },
       {
         id: 27,
@@ -203,32 +218,35 @@ const SHOP_DATA = [
   },
   {
     id: 5,
-    title: "Mens",
-    routeName: "mens",
+    title: "Other Popular Products",
+    routeName: "popular",
     items: [
       {
         id: 30,
-        name: "Camo Down Vest",
-        imageUrl: "https://i.ibb.co/xJS0T3Y/camo-vest.png",
-        price: 325,
+        name: "Fresh Black Team Instant Perfecting Mask",
+        imageUrl:
+          "https://www.fresh.com/dw/image/v2/BDJQ_PRD/on/demandware.static/-/Sites-fresh_master_catalog/default/dw46e618c2/Products/1466_BlackTeaInstantPerfectingMask_packshot.jpg?sw=1000&sh=1000",
+        price: 30,
       },
       {
         id: 31,
-        name: "Floral T-shirt",
-        imageUrl: "https://i.ibb.co/qMQ75QZ/floral-shirt.png",
-        price: 20,
+        name: "Skin Perfecting 2% BHA Liquid",
+        imageUrl: "https://skincity.fi/bilder/artiklar/zoom/1611017_1.jpg",
+        price: 11,
       },
       {
         id: 32,
-        name: "Black & White Longsleeve",
-        imageUrl: "https://i.ibb.co/55z32tw/long-sleeve.png",
-        price: 25,
+        name: "T.L.C. Framboos Glycolic Night Serum",
+        imageUrl:
+          "https://cdn.cultbeauty.com/slots-img/dru/dru001_30ml_drunkelephant_tlcframboosglycolicnightserum_30ml_2_1560x1960-4uzli.jpg",
+        price: 87,
       },
       {
         id: 33,
-        name: "Pink T-shirt",
-        imageUrl: "https://i.ibb.co/RvwnBL8/pink-shirt.png",
-        price: 25,
+        name: "Medik8 C-Tetra",
+        imageUrl:
+          "https://www.medik8.fi/media/catalog/product/cache/95cb36d3254e0a20b33361b06e7c0ce9/c/-/c-tetra_1_1.jpg",
+        price: 52,
       },
       {
         id: 34,
