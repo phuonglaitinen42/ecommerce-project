c30ad9ee3e5af85285dbaaeec073ffaafeaad4e1
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-09-22 15:25:57 +0300

    moved shop data to redux, created shop reducer and seletor

--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,23 +1,18 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./shop.data";
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import CollecitonPreview from "../../components/collection-preview/CollectionPreview";
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollecitonPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+import { selectCollections } from "../../redux/shop/shop.selector";
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollecitonPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(ShopPage);
