57cb48a3b4b710b74a2752e9d4fd3844e21a9e90
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-09-26 22:01:29 +0300

    updated stripe integration and updated title

--- src/pages/checkout/CheckOut.js
+++ src/pages/checkout/CheckOut.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import {
-  selectCartItems,
-  selectCartTotal,
-} from "../../redux/cart/cart.selectors";
+import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
 import "../../styles/CheckOut.scss";
 import CheckoutItem from "../../components/checkout-item/CheckoutItem";
+import StripeButton from "../../components/stripe-button/StripeButton";
 const CheckOut = ({ cartItems, total }) => {
   return (
     <div className="checkout-page">
@@ -34,6 +32,12 @@ const CheckOut = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: ${total} </span>
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVV:123
+      </div>
+      <StripeButton price={total} />
     </div>
   );
 };
